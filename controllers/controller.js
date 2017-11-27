8c561be3b961b248af033f1bcc0f6acc4acdb002
micheal <micheal4s@live.co.uk>
2017-11-27 20:26:38 +0000

    connecting to mongoose db

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,10 @@
+
+var mongoose = require('mangoose');
+
+//connect to the dateabase
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds123136.mlab.com:23136/todolist');
+
+//create a schema - blueprint
 module.exports = function(app){
 
 app.get('/todo', function(req, res){
