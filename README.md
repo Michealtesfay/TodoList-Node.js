58db72e361d2566232db730407cb8bd01363556e
micheal <michealtesfay@gmail.com>
2018-02-05 09:24:00 +0000

    added readme file

---
+++ README.md
@@ -0,0 +1,17 @@
+# TodoList
+
+The TodoList app is a single page app
+
+
+# Languages used
+
+# JavaScript
+# HTML5/CSS3 
+# Bootstrap v4
+
+# Installation
+
+# body-parser
+# ejs
+# express
+# mongoose
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,20 +1,29 @@
 
-var mongoose = require('mangoose');
+//var mongoose = require('mangoose');
 
 //connect to the dateabase
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds123136.mlab.com:23136/todolist');
+//mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds123136.mlab.com:23136/todolist');
 
 //create a schema - blueprint
+var bodyParser = require('body-parser');
+
+var data = [{item: 'get milk'},{item: 'you doing great'},{item:'coding is graet'},{item: 'What are you doing :)'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app){
 
 app.get('/todo', function(req, res){
-  res.render('todo');
+  res.render('todo', {todos: data});
 });
 
-app.post('/todo', function(req, res){
-
+app.post('/todo', urlencodedParser, function(req, res){
+ data.push(req.body);
+ res.json(data);
 });
- app.delete('/todo', function(req, res){
-
+ app.delete('/todo/:item', function(req, res){
+   data = data.filter(function(todo){
+     return todo.item.replace(/ /g, '-') !== req.params.item;
+     res.json(data);
+   });
  });
-};
+}
